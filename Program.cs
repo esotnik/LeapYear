c58a36bd772b15e51deea72ea85f034ab4879ae5
Eve <e_sotnik@bk.ru>
2021-03-23 16:42:42 +0300

    1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace LeapYear
 {
     class Program
     {
+        public static bool IsLeapYear(int y)
+        {
+            return (y % 400) == 0 || (y % 4) == 0 && (y % 100) != 0;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsLeapYear(2020));
         }
     }
 }
